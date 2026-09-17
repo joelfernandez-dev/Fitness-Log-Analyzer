23df70fa6d661adeea9d9b5de28b9cb22c01cc40
Joel David Fernandez Carrizales <jdavidf0307@gmail.com>
2026-09-17 14:25:18 -0400

    Input,ArrayLst,Print

--- src/com/fitnesslog/FitnessLogAnalyzer.java
+++ src/com/fitnesslog/FitnessLogAnalyzer.java
@@ -1,9 +1,29 @@
 package com.fitnesslog;
 
+import java.util.Scanner;
+
 public class FitnessLogAnalyzer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Enter exercises separated by commas:");
+		String exercises = scnr.nextLine(); //this will read the input of the user 
+		
+		String[] exercisesLst = exercises.split(",");
+//		python example on how split works: 
+//		"hello world".split() # Output: ['hello', 'world'] SPLITTING BY WHITESPACE
+//		"apple,banana,cherry".split(",") # Output: ['apple', 'banana', 'cherry'] SPLITTING BY COMA
+		
+		for (int i=0; i<exercisesLst.length; i++) {
+//			exercisesLst.length gives out the length count of the items in the array
+			exercisesLst[i] = exercisesLst[i].trim();
+			System.out.println(exercisesLst[i]);
+			
+		}
+		
+		
+		
 		
 
 	}
